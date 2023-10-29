f697a19dd8158465630f3fcc62289139ad4b98ef
Nads211 <nadia.baptista1999@gmail.com>
2023-10-29 20:57:53 +0000

    Tidy up

--- milestone_3.py
+++ milestone_3.py
@@ -4,8 +4,9 @@ import random
 #Create word list and select a random word
 word_list = ['apple', 'pear', 'orange', 'banana', 'grapes']
 word = random.choice(word_list)
-print(word)
+print("Target word is: " + word)
 
+#define function to check if the guessed letter is in the target word
 def check_guess(guess):
     guess = guess.lower()
     if guess in word:
@@ -13,14 +14,16 @@ def check_guess(guess):
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
-#Iteratively check if the input is a valid guess
+
 def ask_for_input():
+    #Iteratively check if the input is a valid guess
     while True:
         guess = input('Guess a letter: ')
         if len(guess)==1 and guess.isalpha():
             break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
+    #check if guess is in word
     check_guess(guess)
 
 ask_for_input()
