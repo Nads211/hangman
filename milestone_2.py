bffc44ce418a8e7cd44f7f6794fab4d4e7c8ca29
Nads211 <nadia.baptista1999@gmail.com>
2023-10-29 20:08:11 +0000

    Finish milestone_2, adding validation

--- milestone_2.py
+++ milestone_2.py
@@ -4,11 +4,14 @@
 import random
 
 word_list = ['apple', 'pear', 'orange', 'banana', 'grapes']
-
-#print(word_list)
-
+print(word_list)
 word = random.choice(word_list)
 print(word)
 
-guess = input()
-#comment
+guess = input('Guess the first letter: ')
+
+if len(guess)==1 and guess.isalpha():
+    print('Good Guess')
+else:
+    print('Oops! That is not a valid input')
+
