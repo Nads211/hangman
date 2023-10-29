4f2985d7ab7cb2a09f49ec0182bffd0b22c798d6
Nads211 <148590835+Nads211@users.noreply.github.com>
2023-10-29 19:42:18 +0000

    Add first file via upload

---
+++ milestone_2.py
@@ -0,0 +1,13 @@
+#Task 1: Define a list of possible words
+#Task 2: Choose random word
+
+import random
+
+word_list = ['apple', 'pear', 'orange', 'banana', 'grapes']
+
+#print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input()
